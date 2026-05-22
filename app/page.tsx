afced4d1b8bce866d2ae278bfc336d87509d8df7
Shivam Gupta <guptashivam251095@gmail.com>
2026-05-22 21:54:16 +0530

    temporary safe deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,23 @@
-import Layout from "@/components/Layout";
-import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Stats from "@/components/Stats";
-import Expertise from "@/components/Expertise";
-import Achievements from "@/components/Achievements";
-import Timeline from "@/components/Timeline";
-import Leadership from "@/components/Leadership";
-import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
 
   return (
 
-    <Layout>
+    <main
+      style={{
+        background: "#050816",
+        color: "white",
+        minHeight: "100vh"
+      }}
+    >
 
-      {/* NAVBAR */}
-      <Navbar />
+      <Hero />
 
-      {/* HERO */}
-      <div id="home">
-        <Hero />
-      </div>
-
-      {/* ABOUT */}
-      <div id="about">
-        <About />
-      </div>
-
-      {/* STATS */}
-      <div id="stats">
-        <Stats />
-      </div>
-
-      {/* EXPERTISE */}
-      <div id="expertise">
-        <Expertise />
-      </div>
-
-      {/* ACHIEVEMENTS */}
-      <div id="achievements">
-        <Achievements />
-      </div>
-
-      {/* TIMELINE */}
-      <div id="journey">
-        <Timeline />
-      </div>
-
-      {/* LEADERSHIP */}
-      <div id="leadership">
-        <Leadership />
-      </div>
-
-      {/* CONTACT */}
-      <div id="contact">
-        <Contact />
-      </div>
-
-      {/* FOOTER */}
       <Footer />
 
-    </Layout>
+    </main>
 
   );
 }
