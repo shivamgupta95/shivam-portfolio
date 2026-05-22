ea48c6d1e97c96d50d64c19d34e1ca329500d5b4
Shivam Gupta <guptashivam251095@gmail.com>
2026-05-22 21:37:36 +0530

    fixed fadeup component

---
+++ components/FadeUp.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function FadeUp({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+
+    <motion.div
+
+      initial={{
+        opacity: 0,
+        y: 80
+      }}
+
+      whileInView={{
+        opacity: 1,
+        y: 0
+      }}
+
+      transition={{
+        duration: 0.8,
+        ease: "easeOut"
+      }}
+
+      viewport={{
+        once: true
+      }}
+    >
+
+      {children}
+
+    </motion.div>
+
+  );
+}
