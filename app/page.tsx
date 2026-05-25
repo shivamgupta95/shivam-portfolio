aff128232697b2ab60cbcf1897e2e15c9c1a6989
Shivam Gupta <guptashivam251095@gmail.com>
2026-05-25 17:19:13 +0530

    floating whatsapp button

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,12 @@ import Timeline from "../components/Timeline";
 import Leadership from "../components/Leadership";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import Whatsapp from "../components/Whatsapp";
 
 export default function Home() {
+
   return (
+
     <main
       style={{
         background: "#050816",
@@ -18,48 +21,44 @@ export default function Home() {
         minHeight: "100vh",
       }}
     >
+
       <Navbar />
 
-      {/* HERO */}
       <Hero />
 
-      {/* ABOUT */}
       <section id="about">
         <About />
       </section>
 
-      {/* STATS */}
       <section id="stats">
         <Stats />
       </section>
 
-      {/* EXPERTISE */}
       <section id="expertise">
         <Expertise />
       </section>
 
-      {/* ACHIEVEMENTS */}
       <section id="achievements">
         <Achievements />
       </section>
 
-      {/* TIMELINE */}
       <section id="timeline">
         <Timeline />
       </section>
 
-      {/* LEADERSHIP */}
       <section id="leadership">
         <Leadership />
       </section>
 
-      {/* CONTACT */}
       <section id="contact">
         <Contact />
       </section>
 
-      {/* FOOTER */}
       <Footer />
+
+      <Whatsapp />
+
     </main>
+
   );
 }
